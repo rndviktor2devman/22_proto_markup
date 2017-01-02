100544582fb57babc8755258ff94f38af63490e2
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-01-03 00:23:56 +0300

    some customer's data

--- assemble.py
+++ assemble.py
@@ -13,16 +13,26 @@ comments_data = {
 
 }
 
-company_data = {
-    'name': '',
-    'area': '',
-    'production': '',
-    'address': '',
-    'contact_phone': ''
+partners_data = {
+    'partner_name': 'ООО Строй-Сервис-Монтаж',
+    'partner_area': 'Место деятельности: Новосибирск',
+    'partner_production': 'Продукция: ЖБИ, бетон',
+    'partner_address': 'Адрес: Под часами, на том-же месте',
+    'partner_contact_phone': '00-00-00'
 }
+
+catalog_data = {
+    'user': 'Some user',
+    'title': 'Catalog',
+    'items': 5,
+    'show_footer': True
+}
+
+catalog_data.update(partners_data)
 
 if __name__ == '__main__':
     site = staticjinja.make_site(outpath='static', contexts=[
-        ('index.html', index_data)
+        ('index.html', index_data),
+        ('catalog.html', catalog_data)
     ])
     site.render()
