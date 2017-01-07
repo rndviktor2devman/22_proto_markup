3e03f58f1c98edb85880f9ea3ac84e700ce4936b
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-01-08 00:32:06 +0300

    added requests

--- assemble.py
+++ assemble.py
@@ -6,11 +6,35 @@ index_data = {
     'title': 'Главная | Поиск',
     'requests': 'Свежие заявки',
     'comments': 4,
-    'show_footer': True
+    'show_footer': True,
+    'items': 3,
+    'user_info': 'Кирилл, 29 лет, г. Барабинск',
+    'item_comment': '''Бла-бла, мне помогло, все супер! Бла-бла,
+                мне помогло, все супер! Бла-бла, мне помогло,
+                все супер! Бла-бла, мне помогло, все супер!
+                Бла-бла, мне помогло, все супер!'''
 }
 
 comments_data = {
+    'comment_time': 'вчера, в 12:50',
+    'request_text': '''60шт. ПК от 72-15  до ПК 21-15,
+                        Криводановка, с доставкой.''',
+    'request_name': 'Алексей',
+    'views_count': '9'
+}
 
+requests_data = {
+    'title': 'Заявки',
+    'user': 'Леонид Федорович',
+    'comments': 7,
+    'show_footer': True
+}
+
+private_data = {
+    'user': 'Леонид Федорович',
+    'title': 'Каталог организаций',
+    'items': 7,
+    'show_footer': True
 }
 
 partners_data = {
@@ -28,11 +52,15 @@ catalog_data = {
     'show_footer': True
 }
 
+index_data.update(comments_data)
+requests_data.update(comments_data)
 catalog_data.update(partners_data)
 
 if __name__ == '__main__':
     site = staticjinja.make_site(outpath='static', contexts=[
         ('index.html', index_data),
-        ('catalog.html', catalog_data)
+        ('catalog.html', catalog_data),
+        ('private.html', private_data),
+        ('requests.html', requests_data)
     ])
     site.render()
